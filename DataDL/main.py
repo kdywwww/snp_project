040e08aa3779d84bc8487726e500e95945ee4d1d
kdywwww <kdywwww1@gmail.com>
2025-08-16 10:23:27 +0000

    GCS 버킷 연결 및 관련 기능 수정

--- DataDL/main.py
+++ DataDL/main.py
@@ -1,21 +1,23 @@
-import pandas as pd
-from datetime import datetime, timedelta
-from twelvedata import TDClient
-import os
-from loguru import logger
 import glob
+import os
+import shutil
+from datetime import datetime
+
+import pandas as pd
 import pytz
+from loguru import logger
+from twelvedata import TDClient
 
 # 변수 및 경로 설정
-timestamp_str = datetime.now(pytz.timezone('Asia/Seoul')).strftime("%Y%m%d_%H%M%S")
+timestamp_str = datetime.now(pytz.timezone("Asia/Seoul")).strftime("%Y%m%d_%H%M%S")
 API_KEY = "45d1cfb01d4d4677a1a158bd46a97b84"
 MAX_BACKUP_NUM = 2
 
-data_dir = '/data'
+data_dir = "/bucket/data/raw_data"
 backup_dir = os.path.join(data_dir, "backups")
-twelve_dict = {"SPY": ['spy_data.csv'], 
-               "USD/KRW": ['usdkrw_data.csv']}
+twelve_dict = {"SPY": ["spy_data.csv"], "USD/KRW": ["usdkrw_data.csv"]}
 log_dir = "/logs"
+bucket_log_dir = "/bucket/logs/DataDL"
 log_filename = f"log_datadl_{timestamp_str}.log"
 
 
@@ -23,8 +25,6 @@ log_filename = f"log_datadl_{timestamp_str}.log"
 logger.add(os.path.join(log_dir, log_filename))
 logger.info("Start DataDL.")
 
-# TDClient 초기화
-td = TDClient(apikey=API_KEY)
 
 # 날짜 파일명 생성 함수
 def gen_timestamp_filename(filename, timestamp_str):
@@ -40,6 +40,7 @@ def gen_timestamp_filename(filename, timestamp_str):
     """
     name, ext = os.path.splitext(filename)
     return f"{name}_{timestamp_str}{ext}"
+
 
 # 백업 파일 정리 함수
 def clean_old_backups(file_dir, filename, max_num):
@@ -62,22 +63,30 @@ def clean_old_backups(file_dir, filename, max_num):
             os.remove(file)
             logger.info(f"Delete old backup: {file}")
 
-# TwelveData
-for symbol in twelve_dict.keys():
-    try:
+
+try:
+    # TwelveData
+    td = TDClient(apikey=API_KEY)
+    for symbol in twelve_dict.keys():
         # target
-        logger.info(f"Start: {symbol}")
+        logger.info(f"Start: [TwelveData {symbol}]")
         filename = twelve_dict[symbol][0]
         logger.info(f"[TwelveData {symbol}] filename: {filename}")
         backup_filename = gen_timestamp_filename(filename, timestamp_str)
 
         # Load original data
-        data_df = pd.read_csv(os.path.join(data_dir, filename), parse_dates=['datetime'], index_col='datetime')
+        data_df = pd.read_csv(
+            os.path.join(data_dir, filename),
+            parse_dates=["datetime"],
+            index_col="datetime",
+        )
         data_df = data_df.sort_index()
         logger.info(f"[TwelveData {symbol}] Existing Data: {data_df.shape[0]} rows.")
         last_date = data_df.index.max()
-        start_date_str = last_date.strftime('%Y-%m-%d')
-        logger.info(f"[TwelveData {symbol}] Existing Data last_date: {last_date.strftime('%Y-%m-%d')}, Download start_date: {start_date_str}")
+        start_date_str = last_date.strftime("%Y-%m-%d")
+        logger.info(
+            f"[TwelveData {symbol}] Existing Data last_date: {last_date.strftime('%Y-%m-%d')}, Download start_date: {start_date_str}"
+        )
 
         # Retrieve TwelveData
         retrieval_df = td.time_series(
@@ -87,24 +96,41 @@ for symbol in twelve_dict.keys():
             outputsize=5000,
         ).as_pandas()
         retrieval_df = retrieval_df.sort_index()
-        logger.info(f"[TwelveData {symbol}] Successfully retrieved data: {retrieval_df.shape[0]} rows.")
+        logger.info(
+            f"[TwelveData {symbol}] Successfully retrieved data: {retrieval_df.shape[0]} rows."
+        )
 
         # Concat
         data_df = pd.concat([data_df, retrieval_df], axis=0)
         data_df.reset_index(inplace=True)
-        data_df = data_df.drop_duplicates(subset='datetime', keep='last')
-        data_df = data_df.set_index('datetime')
+        data_df = data_df.drop_duplicates(subset="datetime", keep="last")
+        data_df = data_df.set_index("datetime")
         data_df = data_df.sort_index()
-        logger.info(f"[TwelveData {symbol}] Total data after concat: {data_df.shape[0]} rows.")
+        logger.info(
+            f"[TwelveData {symbol}] Total data after concat: {data_df.shape[0]} rows."
+        )
 
         # Save
         data_df.to_csv(os.path.join(data_dir, filename))
         data_df.to_csv(os.path.join(backup_dir, backup_filename))
         clean_old_backups(backup_dir, filename, MAX_BACKUP_NUM)
-        logger.info(f"[TwelveData {symbol}] Save Data: {os.path.join(data_dir, filename)}")
+        logger.info(
+            f"[TwelveData {symbol}] Save Data: {os.path.join(data_dir, filename)}"
+        )
+    logger.info("End DataDL.")
 
+except Exception as e:
+    logger.error(f"Unexpected Error: {e}")
+    raise e
+
+finally:
+    logger.info(f"Copy log to GCS: {os.path.join(bucket_log_dir, log_filename)}")
+    try:
+        # Copy log to GCS mount path
+        shutil.copy(
+            os.path.join(log_dir, log_filename),
+            os.path.join(bucket_log_dir, log_filename),
+        )
+        logger.info("Success to copy log to GCS.")
     except Exception as e:
-        logger.error(f"Unexpected Error: {e}")
-        raise e
-
-logger.info("End DataDL.")
+        logger.error(f"Failed to copy log to GCS: {e}")
